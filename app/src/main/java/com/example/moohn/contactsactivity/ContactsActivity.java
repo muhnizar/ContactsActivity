54e19cd61ef6c05d980c19079e6e9441d0144a5f
muhnizar <mooh.nizar@yahoo.com>
2016-02-24 05:57:27 +0700

    + update activity to get all contact

--- app/src/main/java/com/example/moohn/contactsactivity/ContactsActivity.java
+++ app/src/main/java/com/example/moohn/contactsactivity/ContactsActivity.java
@@ -1,7 +1,11 @@
 package com.example.moohn.contactsactivity;
 
 import android.app.Activity;
+import android.database.Cursor;
+import android.net.Uri;
 import android.os.Bundle;
+import android.provider.ContactsContract;
+import android.widget.TextView;
 
 public class ContactsActivity extends Activity {
 
@@ -9,5 +13,25 @@ public class ContactsActivity extends Activity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_contacts);
+
+        TextView contactview = (TextView) findViewById(R.id.contactview);
+
+        Cursor cursor = getAllContact();
+
+        while (cursor.moveToNext()){
+           String displayname = cursor.getString(cursor.getColumnIndex(ContactsContract.Data.DISPLAY_NAME));
+           contactview.append("Name : ");
+            contactview.append(displayname);
+            contactview.append("\n");
+        }
+    }
+
+    private Cursor getAllContact(){
+        Uri uri = ContactsContract.Contacts.CONTENT_URI;
+        String[] projection = new String[]{ContactsContract.Contacts._ID, ContactsContract.Contacts.DISPLAY_NAME};
+        String selection = ContactsContract.Contacts.IN_VISIBLE_GROUP + "= '" + ("1") + "'";
+        String sortOrder = ContactsContract.Contacts.DISPLAY_NAME + " COLLATE LOCALIZED ASC";
+
+        return getContentResolver().query(uri, projection, selection, null, sortOrder);
     }
 }
